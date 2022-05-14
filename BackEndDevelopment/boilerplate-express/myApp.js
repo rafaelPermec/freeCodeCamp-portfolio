9ddc8b6aff8fe45b8af2f8c085eb1c01792aabf0
rafaelPermec <geekchef.dev@gmail.com>
2022-05-14 20:47:23 -0300

    :construction: my freeCodeCamp node.js/express lecture

--- BackEndDevelopment/boilerplate-express/myApp.js
+++ BackEndDevelopment/boilerplate-express/myApp.js
@@ -2,33 +2,13 @@ let express = require('express');
 let app = express();
 
 
-const helloWorld = () => console.log('Hello World');
-
-helloWorld();
-
+//const helloWorld = () => console.log('Hello World');
+//helloWorld();
+const absolutePath = __dirname + '/views/index.html';
+  
 app.get('/', (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
