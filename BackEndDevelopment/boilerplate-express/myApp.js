eb60d077a8c218af156c8c1bff1989f7d873b71a
rafaelPermec <geekchef.dev@gmail.com>
2022-05-14 21:00:14 -0300

    :construction: my freeCodeCamp node.js/express lecture - middleware

--- BackEndDevelopment/boilerplate-express/myApp.js
+++ BackEndDevelopment/boilerplate-express/myApp.js
@@ -1,21 +1,14 @@
 let express = require('express');
 let app = express();
 
-
-//const helloWorld = () => console.log('Hello World');
-//helloWorld();
 const absolutePath1 = __dirname + '/views/index.html';
   
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.sendFile(absolutePath1);
 });
 
 const absolutePath2 = __dirname + '/public';
 
 app.use('/public', express.static(absolutePath2));
-
-
-
-
 
  module.exports = app;
